d993a3ae16a7327a6750404b8444c4dda5e84bbe
millimelon <aco2@evergreen.edu>
2025-05-18 17:30:11 -0700

    Attempt at reformating to layout

--- taskpanel/src/layouts/default.vue
+++ taskpanel/src/layouts/default.vue
@@ -1,10 +1,331 @@
 <template>
   <a href="#main-content" class="screen-reader-text skip-to-main-content-link">Skip to main content</a>
-  <v-main>
-    <router-view />
+  <v-main> 
+      <v-container fluid id="taskpanel" class="fill-height d-flex flex-column">
+    <v-dialog v-model="editanum" :persistent="!anumber">
+      <v-card class="ma-auto w-100" max-width="400" prepend-icon="mdi-settings">
+        <v-card-title>Edit A#</v-card-title>
+        <v-card-subtitle>Set your A# to track tasks</v-card-subtitle>
+        <v-card-item>
+          <v-text-field
+            id="anum"
+            ref="anum"
+            :prepend-icon="result"
+            v-model="anumber"
+            @input="anumCheck"
+            @keyup.enter="submitAnum"
+            hint="Enter the A# from your student ID"
+            label="A#"
+          ></v-text-field>
+        </v-card-item>
+        <v-card-actions>
+          <v-btn
+            class="ms-auto"
+            text="Close"
+            v-if="anumber"
+            @click="editanum = false"
+          ></v-btn>
+          <v-spacer></v-spacer>
+          <v-btn class="ms-auto" text="Save" @click="submitAnum"></v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <v-dialog v-model="confirmPunchOut">
+      <v-card class="ma-auto w-100" max-width="400" prepend-icon="mdi-settings">
+        <v-card-title>Punch Out All Active Workers?</v-card-title>
+        <v-card-subtitle>Are you Sure?</v-card-subtitle>
+        <v-card-actions>
+          <v-btn
+            class="ms-auto"
+            text="Cancel"
+            @click="confirmPunchOut = false"
+          ></v-btn>
+          <v-spacer></v-spacer>
+          <v-btn class="ms-auto" text="Confirm" @click="punchOutAll"></v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <v-row id="filters" class="align-self-start d-flex w-100 flex-grow-0">
+      <v-col cols="9" sm="4" md="7">
+        <v-text-field
+          id="search"
+          v-model="search"
+          clearable
+          label="Search"
+          hint="Search for tasks by name or description"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="3" class="mt-2 d-flex d-sm-none">
+        <v-btn variant="tonal">
+          <v-icon>mdi-cog</v-icon>
+          <v-menu activator="parent">
+            <v-list>
+              <v-list-item
+                v-for="setting in userSettings"
+                :title="setting.title"
+                @click="setting.action"
+              ></v-list-item>
+            </v-list>
+          </v-menu>
+        </v-btn>
+      </v-col>
+      <v-col cols="6" sm="3" md="2">
+        <v-combobox
+          clearable
+          chips
+          multiple
+          label="Tags"
+          v-model="selectedTags"
+          :items="taskTags"
+        ></v-combobox>
+      </v-col>
+      <v-col cols="6" sm="3" md="2" class="d-flex align-self-start">
+        <v-switch
+          label="Show All"
+          inset
+          color="secondary"
+          v-model="showall"
+        ></v-switch>
+      </v-col>
+      <v-col cols="1" class="mt-2 d-none d-sm-flex">
+        <v-btn variant="tonal">
+          <v-icon>mdi-cog</v-icon>
+          <v-menu activator="parent">
+            <v-list>
+              <v-list-item
+                v-for="setting in userSettings"
+                :title="setting.title"
+                @click="setting.action"
+              ></v-list-item>
+            </v-list>
+          </v-menu>
+        </v-btn>
+      </v-col>
+    </v-row>
+    <v-row
+      id="main-content"
+      align="center"
+      justify="center"
+      class="d-flex flex-row w-100"
+    >
+    <!-- Router View -->
+    <keepAlive>
+      <RouterView v-slot="{ Component }">
+        <KeepAlive>
+          <component :is="Component" @data-updated="updateWorking" @available-tags="getTags" @select="(taskID: number) => selected = taskID" :anumber="anumber" 
+          :showall="showall" :selected="selected" :search="search" :workingdata="workingdata"/>
+        </KeepAlive>
+      </RouterView>
+    </keepAlive>
+
+    <v-col cols="12">
+        <v-btn
+          class="bigbutton"
+          :class="{ selected: selected == 0 }"
+          variant="tonal"
+          @click="selectTask(0)"
+        >
+          Not Tracking Time
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+  <v-snackbar
+    v-model="snackbar"
+    timeout="2000"
+    location="top"
+    :color="snackcolor"
+  >
+    {{ flash }}
+  </v-snackbar>
   </v-main>
 </template>
 
 <script lang="ts" setup>
-  //
+import router from "@/router";
+
+const route = useRoute();
+const editanum: Ref<boolean> = ref(false);
+const confirmPunchOut: Ref<boolean> = ref(false);
+const loading: Ref<boolean> = ref(false);
+const showall: Ref<boolean> = ref(false);
+const taskTags: Ref<Array<string>> = ref([]);
+const selectedTags: Ref<Array<string>> = ref([]);
+const search: Ref<string> = ref("");
+const anumber: Ref<string> = ref("");
+const hash: Ref<string> = ref("");
+const selected: Ref<number> = ref(0);
+const result: Ref<string> = ref("mdi-form-textbox");
+const snackbar: Ref<boolean> = ref(false);
+const snackcolor: Ref<string> = ref("error");
+const flash: Ref<string> = ref("");
+const workingdata = ref({});
+
+const selectTask = (taskID: number) => {
+  if (selected.value == taskID) {
+    return;
+  }
+  selected.value = taskID;
+  if (selected.value > 0) {
+    clockOn(selected.value);
+  } else {
+    clockOff();
+  }
+};
+
+const updateWorking = async (taskdata: any) => {
+  loading.value = true;
+  try {
+    const response = await fetch(import.meta.env.VITE_API + "/hours/working");
+    if (!response.ok) {
+      console.log(response.status);
+    }
+    taskdata.forEach((task) => {
+      workingdata.value[task.ID] = 0;
+    });
+    const jsondata = await response.json();
+    jsondata.forEach((punch) => {
+      workingdata.value[punch.task_id]++;
+      if (punch.worker.barcode == hash.value) {
+        selected.value = punch.task_id;
+      }
+    });
+  } catch (e) {
+    console.log(e);
+  } finally {
+    loading.value = false;
+  }
+};
+
+const clockOn = async (taskID: number) => {
+  const data = { barcode: anumber.value, task: taskID };
+  const response = await fetch(import.meta.env.VITE_API + "/hours/punch", {
+    method: "POST",
+    credentials: "include",
+    body: JSON.stringify(data),
+  });
+  if (!response.ok) {
+    flash.value = response.statusText;
+    snackbar.value = true;
+    console.log(response);
+  }
+  updateWorking();
+};
+const clockOff = async () => {
+  const data = { barcode: anumber.value };
+  const response = await fetch(import.meta.env.VITE_API + "/hours/punch", {
+    method: "POST",
+    credentials: "include",
+    body: JSON.stringify(data),
+  });
+  if (!response.ok) {
+    flash.value = response.statusText;
+    snackbar.value = true;
+    console.log(response);
+  }
+  updateWorking();
+};
+
+
+const logout = async () => {
+  const response = await fetch(import.meta.env.VITE_API + "/logout", {
+    credentials: "include",
+  });
+  if (!response.ok) {
+    flash.value = response.statusText;
+    snackbar.value = true;
+  } else {
+    router.push("/login");
+  }
+};
+
+const setHash = async () => {
+  const data = { barcode: anumber.value };
+  const response = await fetch(import.meta.env.VITE_API + "/worker/lookup", {
+    method: "POST",
+    credentials: "include",
+    body: JSON.stringify(data),
+  });
+  if (!response.ok) {
+    console.log(response);
+  }
+  const jsondata = await response.json();
+  hash.value = jsondata.barcode;
+};
+
+const submitAnum = () => {
+  if (anumber.value == "") {
+    return;
+  }
+  localStorage.setItem("anumber", anumber.value);
+  editanum.value = false;
+  setHash();
+};
+
+const anumCheck = () => {
+  if (
+    anumber.value.length == 9 &&
+    anumber.value[0] == "A" &&
+    !isNaN(Number(anumber.value.substring(1)))
+  ) {
+    result.value = "mdi-check-circle";
+  } else {
+    result.value = "mdi-form-textbox";
+  }
+};
+
+const userSettings = computed(() => {
+  const userStatus: string = route.meta.userstatus;
+  return settings.value.filter((setting) => setting.users.includes(userStatus));
+});
+
+const punchOutAll = async () => {
+  const response = await fetch(
+    import.meta.env.VITE_API + "/hours/punchoutall",
+    { credentials: "include" },
+  );
+  if (!response.ok) {
+    flash.value = response.statusText;
+    snackbar.value = true;
+  }
+  updateWorking();
+  confirmPunchOut.value = false;
+  selected.value = 0;
+};
+const settings = ref([
+  {
+    title: "Edit A#",
+    action: () => {
+      editanum.value = true;
+    },
+    users: ["worker", "admin"],
+  },
+  {
+    title: "Stop Tracking All",
+    action: () => {
+      confirmPunchOut.value = true;
+    },
+    users: ["admin"],
+  },
+  { title: "Logout", action: logout, users: ["worker", "admin"] },
+]);
+
+const getTags = (tags: Array<string>) => {
+  taskTags.value = tags;
+}
+
+
+onMounted(() => {
+  anumber.value = localStorage.getItem("anumber");
+  if (!anumber.value) {
+    editanum.value = true;
+  } else {
+    anumCheck();
+    setHash();
+  }
+});
+
+
+
+
 </script>
--- taskpanel/src/pages/mobile.vue
+++ taskpanel/src/pages/mobile.vue
@@ -1,139 +1,12 @@
 <template>
-  <v-container fluid id="taskpanel" class="fill-height d-flex flex-column">
-    <v-dialog v-model="editanum" :persistent="!anumber">
-      <v-card class="ma-auto w-100" max-width="400" prepend-icon="mdi-settings">
-        <v-card-title>Edit A#</v-card-title>
-        <v-card-subtitle>Set your A# to track tasks</v-card-subtitle>
-        <v-card-item>
-          <v-text-field
-            id="anum"
-            ref="anum"
-            :prepend-icon="result"
-            v-model="anumber"
-            @input="anumCheck"
-            @keyup.enter="submitAnum"
-            hint="Enter the A# from your student ID"
-            label="A#"
-          ></v-text-field>
-        </v-card-item>
-        <v-card-actions>
-          <v-btn
-            class="ms-auto"
-            text="Close"
-            v-if="anumber"
-            @click="editanum = false"
-          ></v-btn>
-          <v-spacer></v-spacer>
-          <v-btn class="ms-auto" text="Save" @click="submitAnum"></v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-    <v-dialog v-model="confirmPunchOut">
-      <v-card class="ma-auto w-100" max-width="400" prepend-icon="mdi-settings">
-        <v-card-title>Punch Out All Active Workers?</v-card-title>
-        <v-card-subtitle>Are you Sure?</v-card-subtitle>
-        <v-card-actions>
-          <v-btn
-            class="ms-auto"
-            text="Cancel"
-            @click="confirmPunchOut = false"
-          ></v-btn>
-          <v-spacer></v-spacer>
-          <v-btn class="ms-auto" text="Confirm" @click="punchOutAll"></v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-    <v-row id="filters" class="align-self-start d-flex w-100 flex-grow-0">
-      <v-col cols="9" sm="4" md="7">
-        <v-text-field
-          id="search"
-          v-model="search"
-          clearable
-          label="Search"
-          hint="Search for tasks by name or description"
-        ></v-text-field>
-      </v-col>
-      <v-col cols="3" class="mt-2 d-flex d-sm-none">
-        <v-btn variant="tonal">
-          <v-icon>mdi-cog</v-icon>
-          <v-menu activator="parent">
-            <v-list>
-              <v-list-item
-                v-for="setting in userSettings"
-                :title="setting.title"
-                @click="setting.action"
-              ></v-list-item>
-            </v-list>
-          </v-menu>
-        </v-btn>
-      </v-col>
-      <v-col cols="6" sm="3" md="2">
-        <v-combobox
-          clearable
-          chips
-          multiple
-          label="Tags"
-          v-model="selectedTags"
-          :items="tasktags"
-        ></v-combobox>
-      </v-col>
-      <v-col cols="6" sm="3" md="2" class="d-flex align-self-start">
-        <v-switch
-          label="Show All"
-          inset
-          color="secondary"
-          v-model="showall"
-        ></v-switch>
-      </v-col>
-      <v-col cols="1" class="mt-2 d-none d-sm-flex">
-        <v-btn variant="tonal">
-          <v-icon>mdi-cog</v-icon>
-          <v-menu activator="parent">
-            <v-list>
-              <v-list-item
-                v-for="setting in userSettings"
-                :title="setting.title"
-                @click="setting.action"
-              ></v-list-item>
-            </v-list>
-          </v-menu>
-        </v-btn>
-      </v-col>
-    </v-row>
-    <v-row
-      id="main-content"
-      align="center"
-      justify="center"
-      class="d-flex flex-row w-100"
-    >
       <TaskCard
         v-for="task in arealist"
         :task="task"
         :anumber="anumber"
         :working="workingdata[task.ID]"
         :selected="selected == task.ID"
-        @select="selectTask"
+        @select="$emit('select', task.ID)"
       ></TaskCard>
-      <v-col cols="12">
-        <v-btn
-          class="bigbutton"
-          :class="{ selected: selected == 0 }"
-          variant="tonal"
-          @click="selectTask(0)"
-        >
-          Not Tracking Time
-        </v-btn>
-      </v-col>
-    </v-row>
-  </v-container>
-  <v-snackbar
-    v-model="snackbar"
-    timeout="2000"
-    location="top"
-    :color="snackcolor"
-  >
-    {{ flash }}
-  </v-snackbar>
 </template>
 
 <script lang="ts" setup>
@@ -145,9 +18,33 @@ definePage({
     requiresAuth: "true",
   },
 });
+
+const props = defineProps({
+anumber: {
+    type: String,
+    required: true
+},
+showall: {
+    type: Boolean,
+    required: false
+},
+search: {
+    type: String,
+    required: false
+},
+selected: {
+    type: Number,
+    required: false
+},
+workingdata: {
+    type: Object,
+    required: true
+}
+});
+
+
+
 const route = useRoute();
-const editanum: Ref<boolean> = ref(false);
-const confirmPunchOut: Ref<boolean> = ref(false);
 const loading: Ref<boolean> = ref(false);
 const showall: Ref<boolean> = ref(false);
 const selectedTags: Ref<Array<string>> = ref([]);
@@ -155,12 +52,14 @@ const search: Ref<string> = ref("");
 const anumber: Ref<string> = ref("");
 const hash: Ref<string> = ref("");
 const selected: Ref<number> = ref(0);
-const result: Ref<string> = ref("mdi-form-textbox");
-const snackbar: Ref<boolean> = ref(false);
-const snackcolor: Ref<string> = ref("error");
-const flash: Ref<string> = ref("");
 const taskdata = ref({});
 const workingdata = ref({});
+const emit = defineEmits<{
+  (e: 'available-tags', tags: Array<string>): void, 
+  (e: 'select', id: number): void,
+  (e: 'data-updated', data: Object): void
+}>();
+
 const logout = async () => {
   const response = await fetch(import.meta.env.VITE_API + "/logout", {
     credentials: "include",
@@ -172,91 +71,41 @@ const logout = async () => {
     router.push("/login");
   }
 };
-const punchOutAll = async () => {
-  const response = await fetch(
-    import.meta.env.VITE_API + "/hours/punchoutall",
-    { credentials: "include" },
-  );
-  if (!response.ok) {
-    flash.value = response.statusText;
-    snackbar.value = true;
-  }
-  updateWorking();
-  confirmPunchOut.value = false;
-  selected.value = 0;
-};
-const settings = ref([
-  {
-    title: "Edit A#",
-    action: () => {
-      editanum.value = true;
-    },
-    users: ["worker", "admin"],
-  },
-  {
-    title: "Stop Tracking All",
-    action: () => {
-      confirmPunchOut.value = true;
-    },
-    users: ["admin"],
-  },
-  { title: "Logout", action: logout, users: ["worker", "admin"] },
-]);
+watch(props, () => {
+  search.value = props.search;
+  showall.value = props.showall;
+  selected.value = props.selected;
+  anumber.value = props.anumber;
+  workingdata.value = props.workingdata;
+})
 
-const userSettings = computed(() => {
-  const userStatus: string = route.meta.userstatus;
-  return settings.value.filter((setting) => setting.users.includes(userStatus));
-});
-
-const tasktags = computed(() => {
-  const tags: Set<string> = new Set();
-  for (const task of Array.from(taskdata.value)) {
-    for (const tag of task.tags) {
-      tags.add(tag.name);
-    }
-  }
-  return Array.from(tags);
-});
 const arealist = computed(() => {
   let areas = Array.from(taskdata.value);
   areas = areas.filter((area) =>
       area.tags.some((tag) => tag.name ==("Management Unit")),
     );
-  return areas;
-});
-const tasklist = computed(() => {
-  let tasks = Array.from(taskdata.value);
   if (!showall.value) {
-    tasks = tasks.filter((task) => focusFilter.includes(task.ID));
-    tasks.sort((a, b) => focusFilter.indexOf(a.ID) - focusFilter.indexOf(b.ID));
+    areas = areas.filter((area) => focusFilter.includes(area.ID));
+    areas.sort((a, b) => focusFilter.indexOf(a.ID) - focusFilter.indexOf(b.ID));
   } else {
-    tasks.sort((a, b) => a.name.localeCompare(b.name));
+    areas.sort((a, b) => a.name.localeCompare(b.name));
   }
   if (selectedTags.value.length > 0) {
-    tasks = tasks.filter((task) =>
-      task.tags.some((tag) => selectedTags.value.includes(tag.name)),
+    areas = areas.filter((area) =>
+      area.tags.some((tag) => selectedTags.value.includes(tag.name)),
     );
   }
   if (search.value) {
-    tasks = tasks.filter((task) =>
-      (task.name + task.description)
-        .toUpperCase()
-        .includes(search.value.toUpperCase()),
+    areas = areas.filter((area) =>
+    (area.name + area.description)
+      .toUpperCase()
+      .includes(search.value.toUpperCase()),
     );
   }
-  return tasks;
+  return areas;
 });
-const selectTask = (taskID: number) => {
-  if (selected.value == taskID) {
-    return;
-  }
-  selected.value = taskID;
-  if (selected.value > 0) {
-    clockOn(selected.value);
-  } else {
-    clockOff();
-  }
-};
+
+
 const getTasks = async () => {
   loading.value = true;
   try {
@@ -265,109 +114,36 @@ const getTasks = async () => {
       console.log(response.status);
     }
     taskdata.value = await response.json();
-    taskdata.value.forEach((task) => {
-      workingdata.value[task.ID] = 0;
-    });
   } catch (e) {
     console.log(e);
   } finally {
-    updateWorking();
+    emit('data-updated', taskdata.value);
   }
 };
-const updateWorking = async () => {
-  loading.value = true;
-  try {
-    const response = await fetch(import.meta.env.VITE_API + "/hours/working");
-    if (!response.ok) {
-      console.log(response.status);
+
+
+const taskTags = computed(() => {
+  const tags: Set<string> = new Set();
+    console.log(Array.from(taskdata.value));
+  for (const task of Array.from(taskdata.value)) {
+    for (const tag of task.tags) {
+      tags.add(tag.name);
     }
-    taskdata.value.forEach((task) => {
-      workingdata.value[task.ID] = 0;
-    });
-    const jsondata = await response.json();
-    jsondata.forEach((punch) => {
-      workingdata.value[punch.task_id]++;
-      if (punch.worker.barcode == hash.value) {
-        selected.value = punch.task_id;
-      }
-    });
-  } catch (e) {
-    console.log(e);
-  } finally {
-    loading.value = false;
   }
-};
-const submitAnum = () => {
-  if (anumber.value == "") {
-    return;
-  }
-  localStorage.setItem("anumber", anumber.value);
-  editanum.value = false;
-  setHash();
-};
-const anumCheck = () => {
-  if (
-    anumber.value.length == 9 &&
-    anumber.value[0] == "A" &&
-    !isNaN(Number(anumber.value.substring(1)))
-  ) {
-    result.value = "mdi-check-circle";
-  } else {
-    result.value = "mdi-form-textbox";
-  }
-};
-const clockOn = async (taskID: number) => {
-  const data = { barcode: anumber.value, task: taskID };
-  const response = await fetch(import.meta.env.VITE_API + "/hours/punch", {
-    method: "POST",
-    credentials: "include",
-    body: JSON.stringify(data),
-  });
-  if (!response.ok) {
-    flash.value = response.statusText;
-    snackbar.value = true;
-    console.log(response);
-  }
-  updateWorking();
-};
-const clockOff = async () => {
-  const data = { barcode: anumber.value };
-  const response = await fetch(import.meta.env.VITE_API + "/hours/punch", {
-    method: "POST",
-    credentials: "include",
-    body: JSON.stringify(data),
-  });
-  if (!response.ok) {
-    flash.value = response.statusText;
-    snackbar.value = true;
-    console.log(response);
-  }
-  updateWorking();
-};
-const setHash = async () => {
-  const data = { barcode: anumber.value };
-  const response = await fetch(import.meta.env.VITE_API + "/worker/lookup", {
-    method: "POST",
-    credentials: "include",
-    body: JSON.stringify(data),
-  });
-  if (!response.ok) {
-    console.log(response);
-  }
-  const jsondata = await response.json();
-  hash.value = jsondata.barcode;
-};
+  return Array.from(tags);
+});
+
+
+
+
+onBeforeMount(() => {
+  getTasks();
+})
+
 let intervalID;
 onMounted(() => {
-  anumber.value = localStorage.getItem("anumber");
-  if (!anumber.value) {
-    editanum.value = true;
-  } else {
-    anumCheck();
-    setHash();
-  }
-  getTasks();
-  intervalID = setInterval(updateWorking, 60000);
+  emit('available-tags', taskTags.value);
+  intervalID = setInterval(emit('data-updated', taskdata.value), 60000);
 });
 onBeforeUnmount(() => {
   clearInterval(intervalID);
