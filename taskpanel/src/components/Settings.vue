a599746686e895567f49cde59f1dec5bbad82cba
Shawn Bird <absent.bird@gmail.com>
2025-05-20 11:28:52 -0700

    more refactoring

--- taskpanel/src/components/Settings.vue
+++ taskpanel/src/components/Settings.vue
@@ -54,5 +54,23 @@
 </template>
 
 <script lang="ts" setup>
-//
+const anumber: Ref<string> = ref("");
+const anum = useTemplateRef("anum");
+const submitAnum = () => {
+  if (anumber.value == "") {
+    return;
+  }
+  if (selected.value == -1) {
+    clockOff(anumber.value);
+  } else {
+    clockOn(anumber.value, selected.value);
+  }
+  anumber.value = "";
+};
+const anumCheck = (e: event) => {
+  if (anumber.value.length > 8) {
+    submitAnum();
+    e.target.focus();
+  }
+};
 </script>
