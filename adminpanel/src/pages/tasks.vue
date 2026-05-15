d1e801a8f31f8a697ca7e4d8929355858729fd89
Shawn Bird <absent.bird@gmail.com>
2026-05-14 22:43:36 -0700

    fully functional task editing

--- adminpanel/src/pages/tasks.vue
+++ adminpanel/src/pages/tasks.vue
@@ -23,12 +23,12 @@
       <v-checkbox v-model="item.selected" @change="toggleSelected(item)" />
     </template>
     <template v-slot:item.ID="{ value }">
-      <v-btn @click="editTask(value)">Edit</v-btn>
-      <v-btn>Delete</v-btn>
+      <v-btn class="ma-2" @click="editTask(value)">Edit</v-btn>
+      <v-btn class="ma-2" @click="editTask(value, true)">Delete</v-btn>
     </template>
   </v-data-table>
   <div class="d-flex justify-center mb-4">
-    <v-btn>Add Task</v-btn>
+    <v-btn @click="newTask">Add Task</v-btn>
   </div>
 
   <v-dialog v-model="taskDialog" width="500">
@@ -45,12 +45,33 @@
           <v-text-field v-model="atask.description" />
           <h3>Selected</h3>
           <v-checkbox v-model="atask.selected" />
+          <div v-if="edit" class="mt-4">
+            <v-btn color="error" @click="deleteDialog = true">Delete</v-btn>
+          </div>
         </v-card-text>
         <v-card-actions>
-          <v-btn>Save</v-btn>
+          <v-btn color="success" @click="saveActive">Save</v-btn>
           <v-btn @click="taskDialog = false">Cancel</v-btn>
         </v-card-actions>
       </v-card-item>
+    </v-card>
+  </v-dialog>
+
+  <v-dialog v-model="deleteDialog" max-width="500">
+    <v-card
+      prepend-icon="mdi-alert-circle"
+      text="Are you sure you want to delete this task?"
+      title="Confirm Deletion"
+    >
+      <template #actions>
+        <v-spacer></v-spacer>
+
+        <v-btn variant="text" @click="deleteDialog = false"> Cancel </v-btn>
+
+        <v-btn color="error" variant="elevated" @click="deleteTask">
+          Delete
+        </v-btn>
+      </template>
     </v-card>
   </v-dialog>
 </template>
@@ -60,8 +81,9 @@ import apicall from "@/composables/apicall";
 import type { Crop } from "@/types/apibinds";
 
 const taskData: Ref<Array<Task>> = ref(Array());
-const atask: Ref<Task | null> = ref(null);
+const atask: Ref<Task> = ref({} as Task);
 const taskDialog: Ref<boolean> = ref(false);
+const deleteDialog: Ref<boolean> = ref(false);
 const edit = computed(() => atask.value.ID != undefined);
 
 const getTasks = async () => {
@@ -88,9 +110,37 @@ const updateTask = async (task) => {
   await apicall("/task/" + task.ID + "/update", task);
 };
 
-const editTask = (id) => {
-  atask.value = taskData.value.find((task) => task.ID === id);
+const editTask = (id: string, del: boolean) => {
+  atask.value = structuredClone(
+    toRaw(taskData.value.find((task) => task.ID === id)),
+  );
+  if (del) {
+    deleteDialog.value = true;
+  } else {
+    taskDialog.value = true;
+  }
+};
+
+const newTask = () => {
+  atask.value = {} as Task;
   taskDialog.value = true;
+};
+
+const saveActive = async () => {
+  if (atask.value.ID != undefined) {
+    await updateTask(atask.value);
+  } else {
+    await apicall("/task/new", atask.value);
+  }
+  getTasks();
+  taskDialog.value = false;
+};
+
+const deleteTask = async () => {
+  await apicall("/task/" + atask.value.ID + "/delete");
+  getTasks();
+  taskDialog.value = false;
+  deleteDialog.value = false;
 };
 
 const headers = [
