f19803537f244876e904704c14b255ca8d5b85e8
Shawn Bird <shawn@mononoke>
2026-05-19 02:05:54 +0000

    sync

--- adminpanel/src/router/index.ts
+++ adminpanel/src/router/index.ts
@@ -9,6 +9,11 @@ import { createRouter, createWebHistory } from "vue-router/auto";
 import { setupLayouts } from "virtual:generated-layouts";
 import { routes } from "vue-router/auto-routes";
 
+routes.push({
+  path: "/",
+  redirect: "/tasks",
+});
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: setupLayouts(routes),
@@ -48,11 +53,6 @@ router.beforeResolve(async (to) => {
       return { path: "/login" };
     }
   }
-});
-
-routes.push({
-  path: "/",
-  redirect: "/tasks",
 });
 
 export default router;
